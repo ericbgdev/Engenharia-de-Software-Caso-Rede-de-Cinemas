4d172c031ed96146d564685d141ad1deb49894c8
Eric <eric.gudera@sou.unifeob.edu.br>
2026-05-06 20:35:21 -0300

    Update sessao_controller.dart

--- lib/controllers/sessao_controller.dart
+++ lib/controllers/sessao_controller.dart
@@ -1,22 +1,14 @@
 import '../models/sessao.dart';
 import '../repositories/sessao_repository.dart';
 
-// Controller: faz a ponte entre a View e o Repository
-// Aqui ficaria regra de negócio (ex: validações, filtros)
 class SessaoController {
   final SessaoRepository _repository = SessaoRepository();
-
-  // Retorna todas as sessões disponíveis
   Future<List<Sessao>> carregarSessoes() async {
     return await _repository.buscarTodasSessoes();
   }
-
-  // Retorna sessões de uma sala específica
   Future<List<Sessao>> carregarSessoesPorSala(int sala) async {
     return await _repository.buscarSessoesPorSala(sala);
   }
-
-  // Formata a duração de minutos para "Xh Ym"
   String formatarDuracao(int minutos) {
     final horas = minutos ~/ 60;
     final mins = minutos % 60;
