6807d3a9e43f44faec419b88c0a548a19329f81d
Eric <eric.gudera@sou.unifeob.edu.br>
2026-05-06 20:35:59 -0300

    Update filme.dart

--- lib/models/filme.dart
+++ lib/models/filme.dart
@@ -1,11 +1,9 @@
-// Model que representa um filme no banco de dados
 class Filme {
   final int? id;
   final String titulo;
   final String genero;
   final int duracaoMinutos;
   final String descricao;
-
   Filme({
     this.id,
     required this.titulo,
@@ -13,8 +11,6 @@ class Filme {
     required this.duracaoMinutos,
     required this.descricao,
   });
-
-  // Converte objeto para Map (usado ao salvar no SQLite)
   Map<String, dynamic> toMap() {
     return {
       'id': id,
@@ -24,8 +20,6 @@ class Filme {
       'descricao': descricao,
     };
   }
-
-  // Cria objeto a partir de um Map (usado ao ler do SQLite)
   factory Filme.fromMap(Map<String, dynamic> map) {
     return Filme(
       id: map['id'],
